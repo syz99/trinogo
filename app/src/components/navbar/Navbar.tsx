21e539e9d3931c870dcb8265d98ff58229a0a396
Samuel Zhang <samzyao@gmail.com>
2020-09-12 23:03:45 -0400

    navbar structure

--- app/src/components/navbar/Navbar.tsx
+++ app/src/components/navbar/Navbar.tsx
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // import { Navbar as NavBar, Nav, NavLink as Link } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 
 import Logo from 'images/logo.png';
 import './style.css';
 
 const Navbar = () => {
+  const [activePath, setActivePath] = useState(useLocation().pathname);
+
+  const uppdateActivePath = (path: string) => {
+    setActivePath(path);
+  };
+
   return (
     <div className="container">
       <nav>
         <ul className="primary-nav">
-          <li className="left">
+          <li className="nav-logo">
             <a href="/">
               <img src={Logo} id="logo" alt="trinogo logo" />
             </a>
           </li>
-          <li className="right">
+          <li className="nav-item">
             <a href="/leaderboard">leaderboard</a>
           </li>
-          <li className="right">
+          <li className="nav-item">
             <a href="/plan">plan your (not) trip</a>
           </li>
-          <li className="right">
+          <li className="nav-item">
             <a href="/transport">transport me</a>
           </li>
-          <li className="right">
+          <li className="nav-item">
             <a href="/passport">my (expired) passport</a>
           </li>
         </ul>
