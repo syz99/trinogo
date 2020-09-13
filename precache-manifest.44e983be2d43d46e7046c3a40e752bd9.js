3964f66affd794aeb0918a7c6fff5d1fe2ac5646
Samuel Zhang <samzyao@gmail.com>
2020-09-13 00:42:07 -0400

    Updates

---
+++ precache-manifest.44e983be2d43d46e7046c3a40e752bd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493481f9c58e4bb2d216b0183a257aa1",
+    "url": "/trinogo/index.html"
+  },
+  {
+    "revision": "a5b621391d8e8a5ed660",
+    "url": "/trinogo/static/css/main.460354ff.chunk.css"
+  },
+  {
+    "revision": "3735cf0b5a165ccfbf17",
+    "url": "/trinogo/static/js/2.18a3f3f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trinogo/static/js/2.18a3f3f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b621391d8e8a5ed660",
+    "url": "/trinogo/static/js/main.fdf3642d.chunk.js"
+  },
+  {
+    "revision": "badcd2cf61f8930a0ce5",
+    "url": "/trinogo/static/js/runtime-main.4c30b47f.js"
+  },
+  {
+    "revision": "9b394f0d6ba2965f0f509a96eb030c4a",
+    "url": "/trinogo/static/media/Couchplane.9b394f0d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.342ab801a8c76ae42a621ec3ddd36344.js"
+  "/trinogo/precache-manifest.44e983be2d43d46e7046c3a40e752bd9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trinogo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
